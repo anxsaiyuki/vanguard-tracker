06557e0e47905643ed5c44b5f9b62615cdc3c063
Tim Yau <tyau52@gmail.com>
2017-04-09 06:07:14 -0700

    adding config file

--- src/component/option.jsx
+++ src/component/option.jsx
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { POWER_INTERVAL, POWER_LIMIT } from '../config.js';
 
 import '../css/option.css';
 
+
 export default class Option extends Component {
   constructor(){
-    super()
+    super();
+
+
+    this.state = {
+      power: []
+    }
   }
 
   render() {
-    //FOR NOW, 20% WIDTH OF THE WINDOW
-    return (
-      <div id="option">
-        Option
-        <ul>
 
+    let optionItem = this.state.power.map(function(item){
+      return (
+        <li className="optionItem">{item}</li>
+      )
+    });
+
+    return (
+      <div id="optionWrapper">
+        Option
+        <ul id="optionList">
+          {optionItem}
         </ul>
       </div>
     )
