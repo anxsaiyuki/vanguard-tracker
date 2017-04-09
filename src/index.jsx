45d3e62b751a9373b4b598f9b52074674581ac25
Andrew <andrew@Andrews-MacBook-Pro.local>
2017-04-09 05:22:12 -0700

    Add option to component

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Board from './component/board';
+import Option from './component/option';
 
 class Index extends Component {
   constructor(){
@@ -11,6 +12,7 @@ class Index extends Component {
     return (
       <div>
         <Board />
+        <Option />
       </div>
     )
   }
