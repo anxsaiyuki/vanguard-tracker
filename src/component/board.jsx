a0c434d74b06ca2412e1b6c4add196746d8c0702
Andrew <andrew@Andrews-MacBook-Pro.local>
2017-04-09 05:26:13 -0700

    Fix component name issue

--- src/component/board.jsx
+++ src/component/board.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class Board extends Component {
+export default class Board extends Component {
   constructor(){
     super()
   }
---
+++ src/component/option.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Option extends Component {
+  constructor(){
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+        Option
+      </div>
+    )
+  }
+}
+
